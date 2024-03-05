f4494a983838cab0a94b9b15dd3212d71b705d67
Mase <mase@hackthegap.com>
2024-03-05 14:26:30 +0000

    Added assignment details

---
+++ README.md
@@ -0,0 +1,21 @@
+# JavaScript Garden
+
+This week we'll be diving into the world of gardening while learning Javagarden basics. You'll be simulating tasks related to plant care and maintenance using JavaScript variables, conditionals, and loops.
+
+## Assignment
+
+As always, start by forking and cloning this repository from GitHub. Open the code in VSCode to get started.
+
+Remember to open the browser console to see the output of your JavaScript code as you go.
+
+### Files Provided
+
+You'll be working with the following files:
+
+- [ ] *garden.js* (Assignment with instructions can be found here)
+- [ ] *index.html* (You can put any questions you have in this file)
+
+The `garden.js` file contains comments with prompts on what code to write. Do not erase the comments, just write the code below each commented line.
+
+**NOTE:** The `garden.js` file is already linked to `index.html`.
+
---
+++ assignment/scripts/garden.js
@@ -0,0 +1,37 @@
+// 1 - Create a variable called `plantType` and assign it the value of a plant type (e.g., "Tomato", "Rose", "Lavender")
+
+
+// 2 - Create a second variable called `plantHeight` and assign it the value of the plant's height in inches (e.g., 12, 24, 36)
+
+
+// 3 - Create a third variable called `plantLocation` and assign it the value of where the plant is located (e.g., "Backyard", "Balcony", "Indoor")
+
+
+// 4 - Console log the value of each variable
+
+
+// 5 - Create a variable named `isOutdoor` and set it to a boolean value (true or false) indicating whether the plant is outdoors
+
+
+// 6 - Create a variable named `waterFrequency` and set its value to a string describing how often the plant needs watering (e.g., "Daily", "Weekly", "Bi-weekly")
+
+
+// 7 - Create a variable called `daysSinceLastWatering` and set it to the number of days since the plant was last watered
+
+
+// 8 - Create a conditional: if `isOutdoor` is true, console log "Remember to check the weather forecast!"
+
+
+// 9 - Write a conditional that console logs "Water the plant" if the value of `daysSinceLastWatering` is greater than or equal to 2,
+// and console logs "Do not water the plant yet" if the value is less than 2.
+
+
+// 10 - Create a variable called `daysToWater` and set it to 7
+
+
+// 11 - Create a while loop that logs "Water the plant" while `daysToWater` is greater than 0,
+// and decrements `daysToWater` by 1 in each iteration
+
+
+// 12 - Create a for loop that logs "Check for pests" for each day of the week (i.e., 7 times)
+
