bea423e95da8a9a1fdb94e509f8ac357e7889920
Mai <myang012@gmail.com>
2024-03-05 16:24:08 -0600

    update

--- assignment/scripts/garden.js
+++ assignment/scripts/garden.js
@@ -2,14 +2,17 @@
 var plantType = "Tomato";
 var plantType = "Rose";
 var plantType = "Lavender";
+
 // 2 - Create a second variable called `plantHeight` and assign it the value of the plant's height in inches (e.g., 12, 24, 36)
 var plantHeight = "12";
 var plantHeight = "24";
 var plantHeight = "36";
+
 // 3 - Create a third variable called `plantLocation` and assign it the value of where the plant is located (e.g., "Backyard", "Balcony", "Indoor")
 var plantLocation = "Backyard";
 var plantLocation = "Balcony";
 var plantLocation = "Indoor";
+
 // 4 - Console log the value of each variable
 console.log(Tomato);
 console.log(Rose);
@@ -20,17 +23,22 @@ console.log(36);
 console.log(Backyard);
 console.log(Balcony);
 console.log(Indoor);
+
 // 5 - Create a variable named `isOutdoor` and set it to a boolean value (true or false) indicating whether the plant is outdoors
-var is 
+var isOutdoor = true/false;
 
 // 6 - Create a variable named `waterFrequency` and set its value to a string describing how often the plant needs watering (e.g., "Daily", "Weekly", "Bi-weekly")
-
+var waterFrequency = ("How often the plant needs watering" + Daily);
+var waterFrequency = ("How often the plant needs watering" + Weekly);
+var waterFrequency = ("How often the plant needs watering" + Bi-weekly);
 
 // 7 - Create a variable called `daysSinceLastWatering` and set it to the number of days since the plant was last watered
-
+var daysSinceLastWatering = "1"
 
 // 8 - Create a conditional: if `isOutdoor` is true, console log "Remember to check the weather forecast!"
-
+if (isOutdoor){
+    console.log("Remember to check the weather forecast!")
+}
 
 // 9 - Write a conditional that console logs "Water the plant" if the value of `daysSinceLastWatering` is greater than or equal to 2,
 // and console logs "Do not water the plant yet" if the value is less than 2.
